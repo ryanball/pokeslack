cca78aa0db255b8013b1fc219a2e701b15704de0
ryanball <ryancball@gmail.com>
2016-07-26 21:03:28 -0700

    Fix casting

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
     location_name = str(os.environ.get('LOCATION_NAME', env.get('LOCATION_NAME')))
     rarity_limit = int(os.environ.get('RARITY_LIMIT', env.get('RARITY_LIMIT')))
     slack_webhook_url = str(os.environ.get('SLACK_WEBHOOK_URL', env.get('SLACK_WEBHOOK_URL')))
+    step_limit = int(os.environ.get('STEP_LIMIT', env.get('STEP_LIMIT')))
 
     # const vars
     step_size = 0.0025
